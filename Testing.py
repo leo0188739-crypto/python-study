ccf352d891f5d327db6a056e729c9acf4827d9ad
¨Yixoa <Leo0188739@gmail.com>
2026-03-03 10:44:02 +0200

    Python study day1 test

---
+++ Testing.py
@@ -0,0 +1,20 @@
+"""
+比较运算符和逻辑运算符的使用
+
+Version: 1.0
+Author: Leo
+"""
+flag0 = 1 == 1
+flag1 = 3 > 2
+flag2 = 2 < 1
+flag3 = flag1 and flag2
+flag4 = flag1 or flag2
+flag5 = not flag0
+print('flag0 =', flag0)     # flag0 = True
+print('flag1 =', flag1)     # flag1 = True
+print('flag2 =', flag2)     # flag2 = False
+print('flag3 =', flag3)     # flag3 = False
+print('flag4 =', flag4)     # flag4 = True
+print('flag5 =', flag5)     # flag5 = False
+print(flag1 and not flag2)  # True
+print(1 > 2 or 2 == 3)      # False
